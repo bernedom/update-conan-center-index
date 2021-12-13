f9f185672534c3ca6a1c70b15b9892ded316b0de
Dominik Berner <dominik.berner@gmail.com>
2021-12-13 09:48:05 +0100

    Add options to pass user and PAT

--- updater.py
+++ updater.py
@@ -14,15 +14,21 @@ parser.add_argument(
     '--fork', help='The fork of the Conan Center Index', required=True)
 parser.add_argument('--version', help='The version to add', required=True)
 parser.add_argument('--recipe', help='The recipe to update', required=True)
-parser.add_argument('--dryrun', help='Dry run, do not push',
-                    action='store_true')
+parser.add_argument('--access-token', help='The personal access token to use to push the changes', required=True)
+parser.add_argument('--user', help='The user name to use to push the changes', required=True)
 parser.add_argument(
     '--zipball', help='The URL of the archive to add', required=True)
+parser.add_argument('--dryrun', help='Dry run, do not push',
+                    action='store_true')
+
 
 args = parser.parse_args()
 target_dir_ = tempfile.mkdtemp()
 
-repo = Repo.clone_from(args.fork, target_dir_)
+repository_url = "https://{}:{}@github.com/{}.git".format(args.user, args.access_token, args.fork)
+
+
+repo = Repo.clone_from(repository_url, target_dir_)
 branch_name = "{}/{}".format(args.recipe, args.version)
 branch = repo.create_head(branch_name)
 branch.checkout()
@@ -36,7 +42,6 @@ if not os.path.exists(recipe_dir):
 with open(os.path.join(recipe_dir, "configX.yml"), 'r') as stream:
     data_loaded = yaml.safe_load(stream)
 
-print(data_loaded["versions"])
 new_version = dict()
 data_loaded["versions"][args.version] = {'folder': 'all'}
 
@@ -44,7 +49,6 @@ with io.open(os.path.join(recipe_dir, 'config.yml'), 'w', encoding='utf8') as ou
     yaml.dump(data_loaded, outfile,
               default_flow_style=False, allow_unicode=True)
 
-# TODO download correct artifact
 r = requests.get(args.zipball, allow_redirects=True)
 hash = hashlib.sha256(r.content).hexdigest()
 
@@ -54,7 +58,6 @@ with open(os.path.join(recipe_dir, "all", "conandata.yml"), 'r') as stream:
 conandata["sources"][args.version] = {
     'url': args.zipball, 'sha256': hash}
 
-# temporary for debbuging purposes)
 with io.open(os.path.join(recipe_dir, 'conandata.yml'), 'w', encoding='utf8') as outfile:
     yaml.dump(conandata, outfile,
               default_flow_style=False, allow_unicode=True)
